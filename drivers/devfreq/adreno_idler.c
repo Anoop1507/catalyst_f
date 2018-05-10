3f52fab68f3f2fc85b6765caa9e43bed88fbd1f4
Malpa73 <kmktoczek@gmail.com>
2018-05-10 18:50:14 +0200

    Added State_notifier
    Signed-off-by: LordShenron <priyanshupandeydelhi@gmail.com>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -2,6 +2,7 @@
  * Author: Park Ju Hyung aka arter97 <qkrwngud825@gmail.com>
  *
  * Copyright 2015 Park Ju Hyung
+ * Copyright 2016 Joe Maples
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -25,9 +26,6 @@
  * calculating idle frequency(mostly by ondemand's method).
  * The higher frequencies are not touched with this algorithm, so high-demanding
  * games will (most likely) not suffer from worsened performance.
- *
- * The additional idle_lasttime detects if last 500ms was idle before
- * ramping down the frequency to prevent micro-lags on scrolling or playing games.
  */
 
 #include <linux/module.h>
@@ -39,10 +37,12 @@
 #define ADRENO_IDLER_MINOR_VERSION 1
 
 /* stats.busy_time threshold for determining if the given workload is idle.
-   Any workload higher than this will be treated as non-idle workload,
-   meaning the higher it gets, the slower & low-power it would get. */
-static unsigned long idleworkload = 5000;
+   Any workload higher than this will be treated as a non-idle workload.
+   Adreno idler will more actively try to ramp down the frequency
+   if this is set to a higher value. */
+static unsigned long idleworkload = 7000;
 module_param_named(adreno_idler_idleworkload, idleworkload, ulong, 0664);
+
 /* Number of events to wait before ramping down the frequency.
    The idlewait'th events before current one must be all idle before
    Adreno idler ramps down the frequency.
@@ -56,7 +56,7 @@ module_param_named(adreno_idler_idlewait, idlewait, uint, 0664);
 static unsigned int downdifferential = 20;
 module_param_named(adreno_idler_downdifferential, downdifferential, uint, 0664);
 
-/* Master switch to activate whole routine */
+/* Master switch to activate the whole routine */
 static bool adreno_idler_active = true;
 module_param_named(adreno_idler_active, adreno_idler_active, bool, 0664);
 
@@ -67,11 +67,12 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 {
 	if (!adreno_idler_active)
 		return 0;
+
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
 		idlecount++;
 		if (*freq == devfreq->profile->freq_table[devfreq->profile->max_state - 1]) {
-			/* frequency is already at its lowest.
+			/* Frequency is already at its lowest.
 			   No need to calculate things, so bail out. */
 			return 1;
 		}
@@ -81,6 +82,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 			idlecount--;
 			return 1;
+		}
 	} else if (state_suspended) {
 		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
@@ -89,7 +91,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		idlecount = 0;
 		/* Do not return 1 here and allow rest of the algorithm to
 		   figure out the appropriate frequency for current workload.
-		   It can even set it back to lowest frequency. */
+		   It can even set it back to the lowest frequency. */
 	}
 	return 0;
 }
